5c1b051dd859f1c45d00e9735befdd7485a64cad
Jas Delgado <jas@jasdelgado.com>
2019-06-11 21:30:10 -0700

    fix favicon to use mine :)

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,7 +24,7 @@ module.exports = {
         background_color: `#e05e5e`,
         theme_color: `#e05e5e`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/jasicon.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
